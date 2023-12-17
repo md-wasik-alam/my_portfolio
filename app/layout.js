63e771384366af4063b664fde066753e3b723020
wasik alam <wasikalam02@gmail.com>
2023-12-17 19:38:59 +0530

    d

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
         <meta name="google-site-verification" content="G-iIFDdeBUQkvhicF16AMCW9id0I5B48oKI4QcDcpxI" />
         <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css" rel="stylesheet" />
         <meta name="description" content="Welcome to my digital haven! I'm a Full Stack Architect 💻, sculpting interactive realms with Python, JavaScript, PHP, and orchestrating magic with Laravel, MongoDB, MySQL, Next.js, and Node.js. 🌐 In this code-crafted sanctuary, witness my journey as a relentless Web/App alchemist. Each project is a stroke of innovation, a testament to my dedication. Ready to embark on a virtual adventure? Let the lines of code unfold the story of my passion and proficiency! As a skilled backend developer and full-stack developer, I bring expertise in building robust and dynamic web applications using a variety of technologies." />
-        <meta name="keywords" content="Full Stack Developer, Backend Developer, Web Application Developer, Python Developer, JavaScript Developer, PHP Developer, Laravel Developer, MongoDB Developer, MySQL Developer, Next.js Developer, Node.js Developer, Web App Alchemist, Innovation in Web Development, Passionate Coder" />
+        <meta name="keywords" content="Full Stack Developer, Backend Developer, Web Application Developer, Python Developer, JavaScript Developer, PHP Developer, Laravel Developer, MongoDB Developer, MySQL Developer, Next.js Developer, Node.js Developer, Web App Alchemist, Innovation in Web Development, Passionate Coder,Md Wasik Alam, Wasik software developer, wasik Backend developer" />
 
 
 
