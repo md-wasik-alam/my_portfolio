2f7cfa4a6603c50b4b2ee425a08d8778697f03ef
wasik alam <wasikalam02@gmail.com>
2023-12-18 23:02:50 +0530

    skill card add

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Hero from "@/src/components/Hero";
 import Profile from "@/src/components/Profile";
 import Skills from "@/src/components/Skills";
+import Tech from "@/src/components/Tech";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
     <Profile/>
     <Hero/>
     <Skills/>
+    <Tech/>
     </>
   )
 }
