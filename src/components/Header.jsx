627cf50c71d570bc354017b349d635df1e6aacf1
wasik alam <wasikalam02@gmail.com>
2023-12-28 00:08:07 +0530

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-
+'use client'
 import React from 'react'
 import DarkSwitch from './DarkSwitch'
 import { FaLaptopCode } from "react-icons/fa";
@@ -6,6 +6,13 @@ import Link from 'next/link';
 
 
 const Header = () => {
+    const openSetting = () => {
+        // Replace 'your-pdf-file.pdf' with the actual path to your PDF file
+        const pdfPath = './assets/code_profile.code-profile';
+
+        // Open the PDF file in a new browser window or tab
+        window.open(pdfPath, '_blank');
+    };
     return (
         <header>
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
@@ -45,6 +52,9 @@ const Header = () => {
                             </li> */}
                             <li>
                                 <a href="#contact" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                            </li>
+                            <li>
+                                <a href="#" onClick={openSetting} className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Code Config</a>
                             </li>
                          
                             <li>
