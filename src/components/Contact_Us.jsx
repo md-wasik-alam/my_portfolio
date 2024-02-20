bad230bccce0acbd930906468b1e7ac2fd81e741
wasik alam <wasikalam02@gmail.com>
2024-02-20 12:39:58 +0530

    message Box Fixed

--- src/components/Contact_Us.jsx
+++ src/components/Contact_Us.jsx
@@ -81,10 +81,10 @@ const Contact_Us = () => {
                 <div className="lg:w-2/3 md:w-2/3 md:mt-5 lg:mt-5 mt-5 md:gap-2 lg:gap-2 md:p-4 lg:p-4 gap-4 ">
                     <p className="mb-4 lg:mb-0 md:mb-0 text-center lg:text-left md:text-left font-sarif font-semibild text-base lg:text-lg md:text-lg">Fill up the form below to contact me in a very simple way and I will reply to you ASAP. You can also contact me at <span className="badge bg-gray-300 text-black font-light small font-mono">wasikalam02@gmail.com</span></p>
                     <div className="flex flex-col lg:flex-row  md:flex-row w-full gap-5 mt-5">
-                        <input onChange={handleChange} name='name' value={formData.name} type="text" placeholder="Enter your name" className="rounded-xl form-control lg:py-5 md:py-5 py-3 font-light text-sm bg-gray-200 font-poppins md:w-1/2 lg:w-1/2 p-2" />
-                        <input onChange={handleChange} name='email' value={formData.email} type="text" placeholder="Enter your email" className="rounded-xl form-control lg:py-5 md:py-5 py-3 font-light text-sm bg-gray-200 font-poppins md:w-1/2 lg:w-1/2 p-2" />
+                        <input onChange={handleChange} name='name' value={formData.name} type="text" placeholder="Enter your name" className="rounded-xl form-control lg:py-5 md:py-5 py-3 font-light text-sm text-black bg-gray-200 font-poppins md:w-1/2 lg:w-1/2 p-2" />
+                        <input onChange={handleChange} name='email' value={formData.email} type="text" placeholder="Enter your email" className="rounded-xl form-control lg:py-5 md:py-5 py-3 font-light text-sm text-black bg-gray-200 font-poppins md:w-1/2 lg:w-1/2 p-2" />
                     </div>
-                    <textarea onChange={handleChange} name='message' value={formData.message} rows={5} cols={7} placeholder="Enter your message" className="rounded-lg form-control font-light text-sm bg-gray-200 font-poppins mt-3 w-full p-4"></textarea>
+                    <textarea onChange={handleChange} name='message' value={formData.message} rows={5} cols={7} placeholder="Enter your message" className="rounded-lg form-control font-light text-sm text-black bg-gray-200 font-poppins mt-3 w-full p-4"></textarea>
                     <button type='submit' className=" bg-blue-600 hover:bg-blue-800 text-white px-2 py-2 w-full rounded-lg font-semibold font-poppins mt-3"><span>Send Message</span></button>
                 </div>
             </form>
